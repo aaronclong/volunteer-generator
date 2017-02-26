81a3950ac3dac02966a97f84d22d4a41b65488be
Aaron <aaron.long.c@gmail.com>
2017-02-26 18:25:57 -0500

    Origin

---
+++ README.md
@@ -0,0 +1,2 @@
+#Volunteer Generator
+A simple script to easily generate mardown for volunteers contributing to current or future hackathon
---
+++ volunteers.py
@@ -0,0 +1,7 @@
+def read_volunteers():
+    """ Automatic Mark Down Generator for Code for Philly's Hackathon volunteers
+    """
+    input() #clear the first line of csv file
+
+if __name__ == '__main__':
+    read_volunteers()
